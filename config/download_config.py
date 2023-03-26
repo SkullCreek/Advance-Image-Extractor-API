e43390ed330c0b0900a83f09056d78822fccbc9d
Darpanarayan Bahadur <92776555+SkullCreek@users.noreply.github.com>
2023-03-26 19:45:43 +0530

    download model

--- config/download_config.py
+++ config/download_config.py
@@ -2,6 +2,5 @@ from dotenv import dotenv_values
 
 config = dotenv_values("../.env")
 
-cloud_name = config["CLOUDINARY_CLOUD_NAME"]
-api_key = config["CLOUDINARY_API_KEY"]
-api_secret = config["CLOUDINARY_API_SECRET"]
+username = config["MEGA_EMAIL"]
+password = config["MEGA_PASSWORD"]
--- models/download.py
+++ models/download.py
@@ -1,27 +1,55 @@
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
+import os.path
+import urllib.request
+from mega import Mega
+import zipfile
 from config import download_config as dc
 from utilities import utils
 
+
 class DownloadImages:
+    """
+    Create a downloadable link to download all the extracted images
+    """
 
     def __init__(self, urls):
+        """
+        Initialize the cloud storage and custom logger
+        :param urls: set of url of image
+        """
         try:
             self.custom_logger = utils.CustomLogging("download")
             self.custom_logger.initialize_logger('../logs/database.log')
-            self.storage = cloudinary.config(
-                cloud_name=dc.cloud_name,
-                api_key=dc.api_key,
-                api_secret=dc.api_secret,
-                secure=True
-            )
+            self.mega = Mega()
+            self.login = self.mega.login(dc.username, dc.password)
             self.urls = urls
         except Exception as e:
             self.custom_logger.append_message("(download.py(__init__) - " + e.args[0], "exception")
             raise Exception(e)
 
-    def download_image(self):
-        pass
+    def generate_link(self, uuid):
+        """
+        Downloads all the images with the help of urls inside a zip file, uploads it to cloud storage and creates
+        downloadable link :param uuid: Unique id of every job assigned :return:
+        """
+        try:
+            filename = uuid + ".zip"
+            with zipfile.ZipFile(filename, "w", zipfile.ZIP_DEFLATED) as zipf:
+                for url in self.urls:
+                    image_path = os.path.basename(url)
+                    urllib.request.urlretrieve(url, image_path)
+                    zipf.write(image_path)
+                    os.remove(image_path)
 
-a = DownloadImages("abc")
+            self.login.upload(filename)
+            link = self.login.export(filename)
+            os.remove(filename)
+            return link
+        except Exception as e:
+            self.custom_logger.append_message("(download.py(generate_link) - " + e.args[0], "exception")
+            raise Exception(e)
+
+# url = [
+#     "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg",
+#     "https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg"]
+# a = DownloadImages(url)
+# print(a.generate_link("1234"))
